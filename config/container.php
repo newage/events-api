ada45c6e1b9307c9b2c5f20e68c173d5dfdea631
Vadim Leontiev <vadim.leontiev@gmail.com>
2019-05-30 18:37:28 +0200

    Update to new expressive version

--- config/container.php
+++ config/container.php
@@ -1,39 +1,14 @@
 <?php
 
-use Aura\Di\ContainerBuilder;
+declare(strict_types=1);
+
+use Zend\ServiceManager\ServiceManager;
 
 // Load configuration
 $config = require __DIR__ . '/config.php';
 
+$dependencies = $config['dependencies'];
+$dependencies['services']['config'] = $config;
+
 // Build container
-$builder = new ContainerBuilder();
-$container = $builder->newInstance();
-
-// Inject config
-$container->set('config', $config);
-$container->set(
-    Common\Container\ConfigInterface::class,
-    $container->lazyNew(Common\Container\Config::class, [$config])
-);
-
-$container->set(Common\Container\Version::class, $container->lazyNew(Common\Container\Version::class));
-$container->set(
-    Common\Action\VersionMiddleware::class,
-    $container->lazyNew(
-        Common\Action\VersionMiddleware::class,
-        [$container->lazyGet(\Common\Container\Version::class)]
-    )
-);
-
-// Inject factories
-foreach ($config['dependencies']['factories'] as $name => $object) {
-    $container->set($object, $container->lazyNew($object));
-    $container->set($name, $container->lazyGetCall($object, '__invoke', $container));
-}
-
-// Inject invokables
-foreach ($config['dependencies']['invokables'] as $name => $object) {
-    $container->set($name, $container->lazyNew($object));
-}
-
-return $container;
+return new ServiceManager($dependencies);
